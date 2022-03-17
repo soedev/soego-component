3e1a6b91f0ff642895f1ff713133f594c9e0d3fd
soeluc <511576209@qq.com>
2022-03-17 17:13:43 +0800

    code debugging

--- checkgobuild.sh
+++ checkgobuild.sh
@@ -1,4 +1,4 @@
-TARGET_DIR=("ealiyun" "edingtalk" "ees" "eetcd" "egitlab" "egorm" "ehuawei" "ejenkins" "ejira" "ek8s" "ekafka" "elogger" "eminio" "emns" "emongo" "eoauth2" "eredis" "esession" "etoken" "ewechat")
+TARGET_DIR=("ealiyun" "edingtalk" "ees" "eetcd" "egitlab" "egorm"  "ejenkins"  "ek8s" "ekafka" "elogger"  "emns" "emongo" "eoauth2" "eredis" "esession" "etoken" "ewechat")
 ROOT=$(pwd)
 for dir in ${TARGET_DIR[@]}; do
     cd $dir && go build -v ./...
