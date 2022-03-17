82a87ce5d795e938cf6371d1fd4870ad8a760d7b
soeluc <511576209@qq.com>
2022-03-17 14:44:40 +0800

    init ees v0.0.1

---
+++ ees/component.go
@@ -0,0 +1,54 @@
+package ees
+
+import (
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/soedev/soego/core/elog"
+)
+
+const PackageName = "component.ees"
+
+// Component ...
+type Component struct {
+	name   string
+	config *config
+	logger *elog.Component
+	Client *elasticsearch.Client
+}
+
+// New ...
+func newComponent(name string, config *config, logger *elog.Component) *Component {
+	elasticConfig := elasticsearch.Config{
+		Addresses:             config.Addrs,
+		Username:              config.Username,
+		Password:              config.Password,
+		APIKey:                config.APIKey,
+		ServiceToken:          config.ServiceToken,
+		RetryOnStatus:         config.RetryOnStatus,
+		DisableRetry:          !config.EnableRetry,
+		EnableRetryOnTimeout:  config.EnableRetryOnTimeout,
+		MaxRetries:            config.MaxRetries,
+		DiscoverNodesOnStart:  config.EnableDiscoverNodesOnStart,
+		DiscoverNodesInterval: config.DiscoverNodesInterval,
+		EnableMetrics:         config.EnableMetrics,
+		EnableDebugLogger:     config.EnableDebugLogger,
+		DisableMetaHeader:     !config.EnableMetaHeader,
+	}
+
+	if config.EnableTrace {
+		elasticConfig.Transport = NewTransport()
+	}
+
+	client, err := elasticsearch.NewClient(elasticConfig)
+	if err != nil {
+		logger.Panic("component new panic", elog.FieldErr(err))
+	}
+
+	cc := &Component{
+		name:   name,
+		logger: logger,
+		config: config,
+		Client: client,
+	}
+
+	return cc
+}
---
+++ ees/go.mod
@@ -0,0 +1,11 @@
+module github.com/soedev/soego-component/ees
+
+go 1.16
+
+require (
+	github.com/elastic/go-elasticsearch/v8 v8.0.0-20210701131303-a3f8e421ff7c
+	github.com/soedev/soego v0.0.1
+	github.com/spf13/cast v1.3.1
+	go.opentelemetry.io/otel v1.4.1
+	go.opentelemetry.io/otel/trace v1.4.1
+)
